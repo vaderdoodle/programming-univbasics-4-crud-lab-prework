ab9044c3d850c9b9c3c54a47487aaaaf9aa519bd
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-17 19:49:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [apples, pears, bananas, oranges]
+  ["apples", "pears", "bananas", "oranges"]
 end
 
 def add_element_to_end_of_array(array, element)
